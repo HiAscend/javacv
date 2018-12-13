3605072c3ee048d9b49e0279161dd5d26ab509d4
haopandeng <haopandeng@xi-tech.com.cn>
2018-12-13 18:21:06 +0800

    测试代码提交

--- src/main/java/cn/edu/zua/javacv/App.java
+++ src/main/java/cn/edu/zua/javacv/App.java
@@ -1,13 +1,22 @@
 package cn.edu.zua.javacv;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Mat horse = Imgcodecs.imread("/tmp/shine0.jpg");
+        HighGui.imshow("result", horse);
+        HighGui.waitKey();
+        System.exit(0);
+
     }
 }
