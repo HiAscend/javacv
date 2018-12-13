eeb2e115963cfc43b830f79afa59f02701cf08d4
haopandeng <haopandeng@xi-tech.com.cn>
2018-12-13 16:48:54 +0800

    Initial commit

---
+++ src/test/java/cn/edu/zua/javacv/AppTest.java
@@ -0,0 +1,21 @@
+package cn.edu.zua.javacv;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
